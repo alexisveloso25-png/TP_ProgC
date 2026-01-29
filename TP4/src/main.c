c440a0bd396572f8816843441f434aeb9b6d4be6
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 11:53:32 +0100

    Update main.c

--- TP4/src/main.c
+++ TP4/src/main.c
@@ -1,56 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "operator.h"
+#include "fichier.h" // Inclusion du nouvel en-tête
 
-void exercice_4_1() {
-    int n1, n2, res;
-    char op;
-    
-    printf("\n--- Exercice 4.1 : Calcul avec operateurs ---\n");
-    
-    // 1. Affiche le message PUIS attend le premier nombre
-    printf("Entrez num1 : ");
-    fflush(stdout); 
-    scanf("%d", &n1);
-    
-    // 2. Affiche le message PUIS attend le deuxieme nombre
-    printf("Entrez num2 : ");
-    fflush(stdout);
-    scanf("%d", &n2);
-    
-    // 3. Affiche le message PUIS attend le symbole
-    printf("Entrez l'operateur (+, -, *, /, %%, &, |, ~) : ");
-    fflush(stdout);
-    scanf(" %c", &op); // L'espace avant %c est OBLIGATOIRE
+void exercice_4_2() {
+    int choix;
+    char nom_f[100];
+    char msg[256];
 
-    switch (op) {
-        case '+': res = somme(n1, n2); break;
-        case '-': res = difference(n1, n2); break;
-        case '*': res = produit(n1, n2); break;
-        case '/': res = quotient(n1, n2); break;
-        case '%': res = modulo(n1, n2); break;
-        case '&': res = et_bit(n1, n2); break;
-        case '|': res = ou_bit(n1, n2); break;
-        case '~': res = negation_bit(n1); break;
-        default: printf("Erreur : symbole invalide.\n"); return;
+    printf("\nQue souhaitez-vous faire ?\n");
+    printf("1. Lire un fichier\n");
+    printf("2. Ecrire dans un fichier\n");
+    printf("Votre choix : ");
+    fflush(stdout);
+    scanf("%d", &choix);
+    getchar(); // Consomme le retour à la ligne restant
+
+    if (choix == 1) {
+        printf("Entrez le nom du fichier a lire : ");
+        fflush(stdout);
+        scanf("%s", nom_f);
+        lire_fichier(nom_f); //
+    } 
+    else if (choix == 2) {
+        printf("Entrez le nom du fichier : ");
+        fflush(stdout);
+        scanf("%s", nom_f);
+        getchar(); // Nettoyage avant le message
+
+        printf("Entrez le message a ecrire : ");
+        fflush(stdout);
+        fgets(msg, sizeof(msg), stdin); // fgets permet de saisir des espaces
+        
+        ecrire_dans_fichier(nom_f, msg); //
+    } 
+    else {
+        printf("Choix invalide.\n");
     }
-
-    // Affiche enfin le résultat sur une nouvelle ligne
-    printf("Resultat : %d\n", res);
 }
 
 int main() {
     int choix;
-    // Ici aussi, on affiche d'abord la question
-    printf("Quel exercice souhaitez-vous resoudre ?\n");
+    printf("Menu de Travail :\n");
     printf("1. Exercice 4.1 (Calculatrice)\n");
+    printf("2. Exercice 4.2 (Fichiers)\n");
     printf("Votre choix : ");
     fflush(stdout);
+    scanf("%d", &choix);
 
-    if (scanf("%d", &choix) == 1 && choix == 1) {
-        exercice_4_1();
-    } else {
-        printf("Exercice non implemente.\n");
-    }
+    if (choix == 1) exercice_4_1();
+    else if (choix == 2) exercice_4_2(); // Appel de la nouvelle fonction
+    else printf("Choix non valide.\n");
+
     return 0;
 }
