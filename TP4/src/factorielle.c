5344e7abe134a8e58df64e5b6cb839663f7a75e3
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 12:56:15 +0100

    Update factorielle.c

--- TP4/src/factorielle.c
+++ TP4/src/factorielle.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 int factorielle(int num) {
-    if (num == 0) {
-        return 1;
-    } else {
-        return (num * factorielle(num - 1));
-    }
+    if (num <= 0) return 1;
+    int val = num * factorielle(num - 1);
+    return val;
 }
