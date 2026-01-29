80611233565f1fface96be0bf1bc2b7636e3c975
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 17:10:38 +0100

    Update chercherfichier.c

--- TP4/src/chercherfichier.c
+++ TP4/src/chercherfichier.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <string.h>
-
-void exercice_4_6(char *nom_f) {
-    FILE *f = fopen(nom_f, "r");
-    if (!f) { printf("Fichier introuvable.\n"); return; }
+void exercice_4_6(char *fn) {
+    FILE *f = fopen(fn, "r");
+    if (!f) return;
     char p[100], l[256];
-    printf("Phrase a chercher : "); getchar();
-    fgets(p, 100, stdin);
-    p[strcspn(p, "\n")] = 0;
+    printf("\n\x1B[35mRECHERCHE DANS LE FICHIER : %s\x1B[0m\n", fn);
+    printf("Entrez le mot ou phrase à chercher : ");
+    getchar(); fgets(p, 100, stdin); p[strcspn(p, "\n")] = 0;
+    
     int nl = 0;
     while (fgets(l, sizeof(l), f)) {
-        nl++;
-        char *ptr = l; int c = 0;
-        while ((ptr = strstr(ptr, p))) { c++; ptr += strlen(p); }
-        if (c > 0) printf("Ligne %d, %d fois\n", nl, c);
+        nl++; char *ptr = l; int count = 0;
+        while ((ptr = strstr(ptr, p))) { count++; ptr += strlen(p); }
+        if (count > 0) printf("\x1B[32m[Trouvé]\x1B[0m Ligne %d : %d occurrence(s)\n", nl, count);
     }
     fclose(f);
 }
