10b75849d82e0a8fe193ea2e876f6426ef682ef2
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 17:17:56 +0100

    Update liste.c

--- TP4/src/liste.c
+++ TP4/src/liste.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "liste.h"
-
-void init_liste(struct liste_couleurs *l) { l->premier = NULL; }
-void insertion(struct couleur *c, struct liste_couleurs *l) {
-    struct noeud *n = malloc(sizeof(struct noeud));
-    n->data = *c; n->suivant = l->premier; l->premier = n;
+#include "fichier.h"
+void lire_fichier(char *nom) {
+    FILE *f = fopen(nom, "r");
+    if (!f) { printf("\x1B[31mErreur: Impossible d'ouvrir le fichier '%s'\x1B[0m\n", nom); return; }
+    char c;
+    while ((c = fgetc(f)) != EOF) putchar(c);
+    fclose(f);
 }
-void parcours(struct liste_couleurs *l) {
-    struct noeud *act = l->premier;
-    int i = 1;
-    while (act) {
-        printf(" \x1B[36m●\x1B[0m Couleur %02d | \x1B[31mR:%3d\x1B[0m \x1B[32mG:%3d\x1B[0m \x1B[34mB:%3d\x1B[0m\n", 
-                i++, act->data.r, act->data.g, act->data.b);
-        act = act->suivant;
-    }
+void ecrire_dans_fichier(char *nom, char *msg) {
+    FILE *f = fopen(nom, "a");
+    if (f) { fprintf(f, "%s", msg); fclose(f); }
 }
