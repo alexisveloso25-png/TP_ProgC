6d68104c846c197a35a77f571ad817e4ad950d09
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 17:10:17 +0100

    Update factorielle.c

--- TP4/src/factorielle.c
+++ TP4/src/factorielle.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
-int factorielle(int num) {
-    if (num <= 0) return 1;
-    int val = num * factorielle(num - 1);
-    return val;
+int factorielle(int n) {
+    if (n <= 0) return 1;
+    int res = n * factorielle(n - 1);
+    printf("\x1B[30;1m  -> Calcul de %d! en cours...\x1B[0m\n", n);
+    return res;
 }
