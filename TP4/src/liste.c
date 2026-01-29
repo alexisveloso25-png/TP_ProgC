83b2bbfaa5ff6e564300ba19b8c73ff0eaa108d3
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 12:52:32 +0100

    Update liste.c

--- TP4/src/liste.c
+++ TP4/src/liste.c
@@ -15,17 +15,10 @@ void insertion(struct couleur *c, struct liste_couleurs *l) {
 
 void parcours(struct liste_couleurs *l) {
     struct noeud *actuel = l->premier;
+    if (!actuel) printf("Liste vide.\n");
     while (actuel) {
-        printf("RGB(%d,%d,%d)\n", actuel->data.r, actuel->data.g, actuel->data.b);
+        printf("RGB: (%d, %d, %d, %d)\n", 
+                actuel->data.r, actuel->data.g, actuel->data.b, actuel->data.a);
         actuel = actuel->suivant;
     }
-}
-
-void exercice_4_7() {
-    struct liste_couleurs ma_liste;
-    init_liste(&ma_liste);
-    struct couleur rouge = {255, 0, 0, 255};
-    insertion(&rouge, &ma_liste);
-    printf("\n--- LISTE DES COULEURS (4.7) ---\n");
-    parcours(&ma_liste);
 }
