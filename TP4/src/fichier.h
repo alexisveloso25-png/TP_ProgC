85310019d41c84c7d044436ea4a9f44dd7327f59
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 11:59:35 +0100

    Update fichier.h

--- TP4/src/fichier.h
+++ TP4/src/fichier.h
@@ -1,10 +1,11 @@
 #ifndef FICHIER_H
 #define FICHIER_H
 
-// Prototypes pour la gestion de fichiers §.
+// Prototypes pour la gestion de fichiers §
 void lire_fichier(char *nom_de_fichier);
 void ecrire_dans_fichier(char *nom_de_fichier, char *message);
 
 #endif
+
 
 
