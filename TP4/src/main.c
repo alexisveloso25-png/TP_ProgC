26677fa9ab848d06d14713929d56dcde3f862f63
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 12:42:11 +0100

    Update main.c

--- TP4/src/main.c
+++ TP4/src/main.c
@@ -5,100 +5,52 @@
 #include "fichier.h"
 #include "liste.h"
 
-// --- PROTOTYPES DES FONCTIONS EXTERNES ---
-// Ces fonctions sont définies dans leurs fichiers .c respectifs
-void exercice_4_1();           // Dans main.c (voir bas du fichier)
-void exercice_4_2();           // Dans main.c (voir bas du fichier)
-void exercice_4_3();           // DÉFINIE DANS etudiant_bd.c
-int factorielle(int n);        // DÉFINIE DANS factorielle.c
-void exercice_4_6(char *nom);  // DÉFINIE DANS chercherfichier.c
-void exercice_4_7();           // DÉFINIE DANS liste.c
-
-// --- FONCTIONS UTILITAIRES ---
-void afficher_banniere(char *titre) {
-    printf("\n\x1B[36m==============================================\n");
-    printf("   %s\n", titre);
-    printf("==============================================\x1B[0m\n");
-}
+// Prototypes (On prévient le main que les fonctions sont ailleurs)
+void exercice_4_1();
+void exercice_4_2();
+void exercice_4_3(); // Pas de code ici, juste le nom !
+int factorielle(int n);
+void exercice_4_6(char *nom);
+void exercice_4_7();
 
 int main(int argc, char *argv[]) {
-    // Gestion de l'exercice 4.6 via argument de ligne de commande
-    if (argc == 2) {
-        exercice_4_6(argv[1]);
-        return 0;
-    }
+    if (argc == 2) { exercice_4_6(argv[1]); return 0; }
 
     int choix;
     while (1) {
-        afficher_banniere("MENU TP4 - GROUPE A (Alexis, Salmane, Kais, Mohamed)");
-        printf(" 1. Calculatrice Interactive (4.1)\n");
-        printf(" 2. Gestion Fichiers Simple (4.2)\n");
-        printf(" 3. Base de Donnees Etudiants (4.3)\n");
-        printf(" 4. Aide : Calculatrice Ligne de Commande (4.4)\n");
-        printf(" 5. Factorielle Recursive (4.5)\n");
-        printf(" 6. Recherche de phrase dans fichier (4.6)\n");
-        printf(" 7. Liste de couleurs RGB (4.7)\n");
-        printf(" 0. Quitter\n");
-        printf("\n\x1B[33m> Votre choix : \x1B[0m");
-        fflush(stdout);
+        printf("\n--- MENU TP4 (Alexis, Salmane, Kais, Mohamed) ---\n");
+        printf("1. Calculatrice | 2. Fichiers | 3. Base Etudiants\n");
+        printf("5. Factorielle  | 6. Recherche | 7. Liste Couleurs | 0. Quitter\n");
+        printf("Choix : "); fflush(stdout);
 
         if (scanf("%d", &choix) != 1 || choix == 0) break;
 
         switch (choix) {
             case 1: exercice_4_1(); break;
             case 2: exercice_4_2(); break;
-            case 3: exercice_4_3(); break; // Appelle la fonction dans etudiant_bd.c
-            case 4: 
-                printf("\n[INFO] Pour l'exo 4.4, compilez 'calcule.c' et lancez :\n");
-                printf("Usage: ./calcule + 10 5\n");
-                break;
+            case 3: exercice_4_3(); break; // Appelle la fonction de etudiant_bd.c
             case 5: {
-                int n; printf("Entrez un nombre : "); scanf("%d", &n);
-                if(n >= 0) printf("Resultat de %d! : \x1B[32m%d\x1B[0m\n", n, factorielle(n));
+                int n; printf("Nombre : "); scanf("%d", &n);
+                if(n >= 0) printf("Resultat : %d\n", factorielle(n));
                 break;
             }
             case 6: {
-                char nom[50]; printf("Nom du fichier source : "); scanf("%s", nom);
-                exercice_4_6(nom);
-                break;
+                char f[50]; printf("Fichier : "); scanf("%s", f);
+                exercice_4_6(f); break;
             }
             case 7: exercice_4_7(); break;
-            default: printf("\x1B[31mChoix invalide.\x1B[0m\n");
         }
-        printf("\nAppuyez sur Entree pour revenir au menu...");
-        getchar(); getchar(); 
+        printf("\nAppuyez sur Entree pour continuer...");
+        getchar(); getchar();
     }
-
-    printf("\nAu revoir !\n");
     return 0;
 }
 
-// --- IMPLÉMENTATIONS DES EXERCICES INTERACTIFS ---
-
+// --- Garde ici SEULEMENT exercice_4_1 et exercice_4_2 si elles ne sont pas dans d'autres fichiers ---
 void exercice_4_1() {
-    int n1, n2, res; char op;
-    printf("\n--- CALCULATRICE (4.1) ---\n");
-    printf("Operation (ex: 10 + 5) : "); fflush(stdout);
-    scanf("%d %c %d", &n1, &op, &n2);
-    
-    if (op == '+') res = somme(n1, n2); 
-    else if (op == '-') res = difference(n1, n2);
-    else if (op == '*') res = produit(n1, n2);
-    else if (op == '/') res = quotient(n1, n2);
-    else { printf("Operateur non gere ici.\n"); return; }
-    
-    printf("\x1B[32m>>> Resultat : %d\x1B[0m\n", res);
+    // Ton code pour la calculatrice interactive...
 }
 
 void exercice_4_2() {
-    int mode; char nom[50], msg[100];
-    printf("\n--- FICHIERS (4.2) ---\n1. Lire | 2. Ecrire : ");
-    scanf("%d", &mode);
-    printf("Nom du fichier : "); scanf("%s", nom);
-    if (mode == 1) lire_fichier(nom);
-    else {
-        printf("Message : "); getchar();
-        fgets(msg, 100, stdin);
-        ecrire_dans_fichier(nom, msg);
-    }
+    // Ton code pour lire/écrire un fichier...
 }
