6603486551cec37dcba8555cc46db2525a187db8
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 11:43:14 +0100

    Update main.c

--- TP4/src/main.c
+++ TP4/src/main.c
@@ -1,36 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "operator.h"
 
-int main() {
-    int n1, n2, resultat;
+// --- Entrée pour l'exercice 4.1 ---
+void exercice_4_1() {
+    int n1, n2, res;
     char op;
-
-    printf("--- Calculatrice de Groupe ---\n");
+    printf("\n--- Exercice 4.1 : Calcul avec opérateurs ---\n");
     printf("Entrez num1 : ");
     scanf("%d", &n1);
     printf("Entrez num2 : ");
     scanf("%d", &n2);
     printf("Entrez l'operateur (+, -, *, /, %%, &, |, ~) : ");
-    scanf(" %c", &op); // L'espace avant %c est crucial pour ignorer l'Entrée précédente
+    scanf(" %c", &op);
 
     switch (op) {
-        case '+': resultat = somme(n1, n2); break;
-        case '-': resultat = difference(n1, n2); break;
-        case '*': resultat = produit(n1, n2); break;
-        case '/': resultat = quotient(n1, n2); break;
-        case '%': resultat = modulo(n1, n2); break;
-        case '&': resultat = et_bit(n1, n2); break;
-        case '|': resultat = ou_bit(n1, n2); break;
-        case '~': 
-            resultat = negation_bit(n1); 
-            printf("Note : La negation (~) s'applique uniquement sur num1 (%d).\n", n1);
-            break;
-        default:
-            printf("Operateur inconnu !\n");
-            return 1;
+        case '+': res = somme(n1, n2); break;
+        case '-': res = difference(n1, n2); break;
+        case '*': res = produit(n1, n2); break;
+        case '/': res = quotient(n1, n2); break;
+        case '%': res = modulo(n1, n2); break;
+        case '&': res = et_bit(n1, n2); break;
+        case '|': res = ou_bit(n1, n2); break;
+        case '~': res = negation_bit(n1); break;
+        default: printf("Operateur invalide.\n"); return;
     }
+    printf("Resultat : %d\n", res);
+}
 
-    printf("Resultat : %d\n", resultat);
+// --- Placeholders pour les exercices futurs ---
+void exercice_4_2() { printf("\nExercice 4.2 non encore implemente.\n"); }
+void exercice_4_7() { printf("\nExercice 4.7 non encore implemente.\n"); }
+
+int main() {
+    int choix;
+
+    printf("Quel exercice souhaitez-vous resoudre ?\n");
+    printf("1. Exercice 4.1 (Calculatrice)\n");
+    printf("2. Exercice 4.2\n");
+    printf("3. Exercice 4.7\n");
+    printf("Votre choix : ");
+    scanf("%d", &choix);
+
+    switch (choix) {
+        case 1: exercice_4_1(); break;
+        case 2: exercice_4_2(); break;
+        case 3: exercice_4_7(); break;
+        default: printf("Choix indisponible.\n"); break;
+    }
 
     return 0;
 }
