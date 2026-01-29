c441024b301bfdcba5edf075ad64cfe43de8a6c3
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 17:30:15 +0100

    Update chercherfichier.c

--- TP4/src/chercherfichier.c
+++ TP4/src/chercherfichier.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
 #include <string.h>
-void exercice_4_6(char *fn) {
-    FILE *f = fopen(fn, "r");
-    if (!f) return;
-    char p[100], l[256];
-    printf("\n\x1B[35mRECHERCHE DANS LE FICHIER : %s\x1B[0m\n", fn);
-    printf("Entrez le mot ou phrase à chercher : ");
-    getchar(); fgets(p, 100, stdin); p[strcspn(p, "\n")] = 0;
-    
-    int nl = 0;
-    while (fgets(l, sizeof(l), f)) {
-        nl++; char *ptr = l; int count = 0;
-        while ((ptr = strstr(ptr, p))) { count++; ptr += strlen(p); }
-        if (count > 0) printf("\x1B[32m[Trouvé]\x1B[0m Ligne %d : %d occurrence(s)\n", nl, count);
+
+void exercice_4_6(char *nom_f) {
+    FILE *f = fopen(nom_f, "r");
+    if (!f) {
+        printf("\x1B[31m[Erreur] : Impossible d'ouvrir le fichier '%s'. Verifiez qu'il existe.\x1B[0m\n", nom_f);
+        return;
     }
+    
+    char mot[50], ligne[256];
+    printf("\n--- RECHERCHE DE MOT DANS %s ---\n", nom_f);
+    printf("Tapez le mot exact a rechercher : ");
+    fflush(stdout);
+    scanf("%s", mot);
+    
+    int num_ligne = 1;
+    int trouve = 0;
+    
+    while (fgets(ligne, sizeof(ligne), f)) {
+        // strstr cherche le mot dans la ligne
+        if (strstr(ligne, mot)) {
+            printf("\x1B[32m[Trouve]\x1B[0m Ligne %d : %s", num_ligne, ligne);
+            trouve = 1;
+        }
+        num_ligne++;
+    }
+    
+    if (!trouve) {
+        printf("\x1B[33mLe mot '%s' n'a pas ete trouve dans le fichier.\x1B[0m\n", mot);
+    }
+    
     fclose(f);
 }
