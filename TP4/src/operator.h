cb3b50450ca51e625a453bde6c8b2e2008ddb078
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 11:39:55 +0100

    Update operator.h

---
+++ TP4/src/operator.h
@@ -0,0 +1,14 @@
+#ifndef OPERATOR_H
+#define OPERATOR_H
+
+// Prototypes des fonctions de calcul
+int somme(int num1, int num2);
+int difference(int num1, int num2);
+int produit(int num1, int num2);
+int quotient(int num1, int num2);
+int modulo(int num1, int num2);
+int et_bit(int num1, int num2);
+int ou_bit(int num1, int num2);
+int negation_bit(int num1); // La négation ne prend qu'un seul argument
+
+#endif
