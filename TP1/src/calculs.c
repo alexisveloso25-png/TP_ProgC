2187d06fb06d25c2502eb1706d17fdacec2b57e3
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 10:31:03 +0100

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,32 +1,53 @@
 #include <stdio.h>
 
 int main() {
-    int somme = 0;
-    int limite_somme = 5000;
-    int i;
+    int num1 = 10;
+    int num2 = 3;
+    char op = '&'; // Changez ce caractère pour tester d'autres opérations
 
-    for (i = 1; i <= 1000; i++) {
-        // Condition 1 : Divisible par 11 ? On passe au suivant.
-        // On utilise 'continue' pour sauter tout le reste du bloc for.
-        if (i % 11 == 0) {
-            continue;
-        }
+    printf("Opération demandée : %d %c %d\n", num1, op, num2);
 
-        // Condition 2 : Divisible par 5 OU par 7 ?
-        if (i % 5 == 0 || i % 7 == 0) {
-            somme += i;
-        }
-
-        // Condition 3 : Si la somme dépasse 5000, on arrête tout.
-        if (somme > limite_somme) {
-            printf("Arrêt : La somme a dépassé %d (i = %d)\n", limite_somme, i);
+    switch (op) {
+        // --- Opérateurs Arithmétiques ---
+        case '+':
+            printf("Résultat : %d\n", num1 + num2);
             break;
-        }
+        case '-':
+            printf("Résultat : %d\n", num1 - num2);
+            break;
+        case '*':
+            printf("Résultat : %d\n", num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0)
+                printf("Résultat : %d\n", num1 / num2);
+            else
+                printf("Erreur : Division par zéro !\n");
+            break;
+        case '%':
+            if (num2 != 0)
+                printf("Résultat : %d\n", num1 % num2);
+            else
+                printf("Erreur : Modulo par zéro !\n");
+            break;
+
+        // --- Opérateurs de Bits (Bitwise) ---
+        case '&':
+            printf("Résultat (ET bit à bit) : %d\n", num1 & num2);
+            break;
+        case '|':
+            printf("Résultat (OU bit à bit) : %d\n", num1 | num2);
+            break;
+        case '~':
+            // L'opérateur NOT est unitaire (il ne s'applique qu'à un seul nombre)
+            printf("Résultat (NON bit à bit sur num1) : %d\n", ~num1);
+            break;
+
+        // Cas par défaut si l'opérateur n'est pas reconnu
+        default:
+            printf("Erreur : Opérateur '%c' non reconnu.\n", op);
+            break;
     }
 
-    printf("La somme finale est : %d\n", somme);
-
-    return 0; 
+    return 0;
 }
-#
-
