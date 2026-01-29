0f6b86e336ce3566fda2e309c87a3954e549b401
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 12:05:25 +0100

    Update operator.c

--- TP4/src/operator.c
+++ TP4/src/operator.c
@@ -1,5 +1,5 @@
 #include "operator.h"
-// affichage 
+
 int somme(int num1, int num2) { return num1 + num2; }
 int difference(int num1, int num2) { return num1 - num2; }
 int produit(int num1, int num2) { return num1 * num2; }
@@ -8,4 +8,3 @@ int modulo(int num1, int num2) { return (num2 != 0) ? (num1 % num2) : 0; }
 int et_bit(int num1, int num2) { return num1 & num2; }
 int ou_bit(int num1, int num2) { return num1 | num2; }
 int negation_bit(int num1) { return ~num1; }
-
