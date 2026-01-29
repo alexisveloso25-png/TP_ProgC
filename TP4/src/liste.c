c32725698c5adcf4a34e66a0814c3379c3ad7b57
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 12:44:54 +0100

    Update liste.c

--- TP4/src/liste.c
+++ TP4/src/liste.c
@@ -1,26 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "liste.h"
 
-void init_liste(struct liste_couleurs *l) {
-    l->premier = NULL;
-}
-
-void insertion(struct couleur *c, struct liste_couleurs *l) {
-    struct noeud *nouveau = malloc(sizeof(struct noeud));
-    if (nouveau == NULL) return;
-
-    nouveau->data = *c;
-    nouveau->suivant = l->premier; // On insère au début
-    l->premier = nouveau;
-}
-
-void parcours(struct liste_couleurs *l) {
-    struct noeud *actuel = l->premier;
-    int i = 1;
-    while (actuel != NULL) {
-        printf("Couleur %d : R=0x%02X, G=0x%02X, B=0x%02X, A=0x%02X\n", 
-                i++, actuel->data.r, actuel->data.g, actuel->data.b, actuel->data.a);
-        actuel = actuel->suivant;
-    }
+void exercice_4_7() {
+    struct liste_couleurs ma_liste;
+    init_liste(&ma_liste);
+    printf("\n--- LISTE COULEURS 4.7 ---\n");
+    // Exemple d'insertion
+    struct couleur rouge = {255, 0, 0, 255};
+    insertion(&rouge, &ma_liste);
+    parcours(&ma_liste);
 }
