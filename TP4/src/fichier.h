b2225ffafaa2bfb5f69c5169e73679f361d12c21
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 12:50:52 +0100

    Update fichier.h

--- TP4/src/fichier.h
+++ TP4/src/fichier.h
@@ -1,8 +1,13 @@
-/* --- fichier.h --- */
-#ifndef FICHIER_H
-#define FICHIER_H
+#ifndef OPERATOR_H
+#define OPERATOR_H
 
-void lire_fichier(char *nom);
-void ecrire_dans_fichier(char *nom, char *msg);
+int somme(int num1, int num2);
+int difference(int num1, int num2);
+int produit(int num1, int num2);
+int quotient(int num1, int num2);
+int modulo(int num1, int num2);
+int et_bit(int num1, int num2);
+int ou_bit(int num1, int num2);
+int negation_bit(int num1);
 
 #endif
