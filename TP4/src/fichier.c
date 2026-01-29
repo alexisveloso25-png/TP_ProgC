7e2534896059bbfa9a8b89fe4e3cf44e4b91e947
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 17:09:56 +0100

    Update fichier.c

--- TP4/src/fichier.c
+++ TP4/src/fichier.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 #include "fichier.h"
 
-void lire_fichier(char *nom_de_fichier) {
-    FILE *f = fopen(nom_de_fichier, "r");
-    if (!f) {
-        printf("Erreur : Impossible d'ouvrir %s\n", nom_de_fichier);
-        return;
-    }
+void lire_fichier(char *nom) {
+    FILE *f = fopen(nom, "r");
+    if (!f) { printf("\x1B[31m[!] Erreur: Fichier '%s' introuvable.\x1B[0m\n", nom); return; }
     char c;
+    printf("\n\x1B[33m--- CONTENU DU FICHIER ---\x1B[0m\n");
     while ((c = fgetc(f)) != EOF) putchar(c);
+    printf("\n\x1B[33m--------------------------\x1B[0m\n");
     fclose(f);
 }
 
-void ecrire_dans_fichier(char *nom_de_fichier, char *message) {
-    FILE *f = fopen(nom_de_fichier, "a");
-    if (f) {
-        fprintf(f, "%s", message);
-        fclose(f);
-    }
+void ecrire_dans_fichier(char *nom, char *msg) {
+    FILE *f = fopen(nom, "a");
+    if (f) { fprintf(f, "%s", msg); fclose(f); }
 }
