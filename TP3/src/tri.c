ce04b8c9b90215e5c3287720b7bdaa7542123e5c
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 11:07:25 +0100

    Update tri.c

--- TP3/src/tri.c
+++ TP3/src/tri.c
@@ -9,24 +9,17 @@ int main() {
 
     srand(time(NULL));
 
-    // 1. Remplissage aléatoire du tableau (entre -100 et 100)
+    // 1. Remplissage aléatoire
     for (i = 0; i < n; i++) {
         tableau[i] = (rand() % 201) - 100; 
     }
 
-    // 2. Affichage du tableau non trié
-    printf("--- Tableau de scores (Alexis, Salmane, Kais, Mohamed) ---\n");
-    printf("Tableau non trie :\n");
-    for (i = 0; i < n; i++) {
-        printf("%d ", tableau[i]);
-    }
-    printf("\n\n");
+    printf("--- Système de classement (Alexis, Salmane, Kais, Mohamed) ---\n\n");
 
-    // 3. Algorithme de Tri à Bulles
+    // 2. TRI CROISSANT (Petit -> Grand)
     for (i = 0; i < n - 1; i++) {
         for (j = 0; j < n - i - 1; j++) {
-            // Si l'élément actuel est plus grand que le suivant, on échange
-            if (tableau[j] > tableau[j + 1]) {
+            if (tableau[j] > tableau[j + 1]) { // Signe '>' pour croissant
                 temp = tableau[j];
                 tableau[j] = tableau[j + 1];
                 tableau[j + 1] = temp;
@@ -34,8 +27,25 @@ int main() {
         }
     }
 
-    // 4. Affichage du tableau trié
-    printf("Tableau trie par ordre croissant :\n");
+    printf("Tableau trie par ordre CROISSANT :\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", tableau[i]);
+    }
+    printf("\n\n");
+
+    // 3. TRI DÉCROISSANT (Grand -> Petit)
+    // On reprend le tableau déjà trié et on ré-applique l'algorithme inverse
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (tableau[j] < tableau[j + 1]) { // Signe '<' pour décroissant
+                temp = tableau[j];
+                tableau[j] = tableau[j + 1];
+                tableau[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Tableau trie par ordre DECROISSANT :\n");
     for (i = 0; i < n; i++) {
         printf("%d ", tableau[i]);
     }
