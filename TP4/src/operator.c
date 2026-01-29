8129e4e4a8b64aae5527d179e7358ac2bec2e5a6
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 12:51:39 +0100

    Update operator.c

--- TP4/src/operator.c
+++ TP4/src/operator.c
@@ -1,17 +1,10 @@
-#include <stdio.h>
 #include "operator.h"
 
-// Fonctions de base
-int somme(int a, int b) { return a + b; }
-int difference(int a, int b) { return a - b; }
-int produit(int a, int b) { return a * b; }
-int quotient(int a, int b) { return (b != 0) ? a / b : 0; }
-
-void exercice_4_1() {
-    int n1, n2; char op;
-    printf("\n--- CALCULATRICE 4.1 ---\nEntrez op (ex: 10 + 5) : ");
-    scanf("%d %c %d", &n1, &op, &n2);
-    if (op == '+') printf("Res : %d\n", somme(n1, n2));
-    else if (op == '*') printf("Res : %d\n", produit(n1, n2));
-    // Ajoute les autres cas ici...
-}
+int somme(int num1, int num2) { return num1 + num2; }
+int difference(int num1, int num2) { return num1 - num2; }
+int produit(int num1, int num2) { return num1 * num2; }
+int quotient(int num1, int num2) { return (num2 != 0) ? num1 / num2 : 0; }
+int modulo(int num1, int num2) { return (num2 != 0) ? num1 % num2 : 0; }
+int et_bit(int num1, int num2) { return num1 & num2; }
+int ou_bit(int num1, int num2) { return num1 | num2; }
+int negation_bit(int num1) { return ~num1; }
