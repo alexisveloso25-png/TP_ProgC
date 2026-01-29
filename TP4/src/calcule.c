c07559b379d7361ff2bf016d58a094d18b2d6cc7
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 12:27:17 +0100

    Update calcule.c

--- TP4/src/calcule.c
+++ TP4/src/calcule.c
@@ -3,49 +3,31 @@
 #include "operator.h"
 
 int main(int argc, char *argv[]) {
-    // argc est le nombre d'arguments. On attend : ./calcule <op> <n1> <n2>
-    // Donc argc doit etre egal a 4 (le nom du programme + 3 arguments)
-    if (argc < 2) {
-        printf("Usage: %s <operateur> <num1> <num2>\n", argv[0]);
+    // ./calcule <op> <n1> <n2>
+    if (argc < 3) {
+        printf("Usage: %s <op> <n1> [n2]\n", argv[0]);
         return 1;
     }
 
-    // Le premier argument apres le nom est l'operateur
     char op = argv[1][0];
-    int n1, n2, res;
+    int n1 = atoi(argv[2]);
+    int res;
 
-    // Pour la negation (~), il n'y a qu'un seul nombre
     if (op == '~') {
-        if (argc != 3) {
-            printf("Usage pour ~ : %s ~ <num1>\n", argv[0]);
-            return 1;
-        }
-        n1 = atoi(argv[2]); // atoi convertit une chaine "10" en entier 10
         res = negation_bit(n1);
-        printf("Resultat : %d\n", res);
-        return 0;
-    }
-
-    // Pour les autres operations, il faut 2 nombres
-    if (argc != 4) {
-        printf("Usage: %s <operateur> <num1> <num2>\n", argv[0]);
-        return 1;
-    }
-
-    n1 = atoi(argv[2]);
-    n2 = atoi(argv[3]);
-
-    switch (op) {
-        case '+': res = somme(n1, n2); break;
-        case '-': res = difference(n1, n2); break;
-        case '*': res = produit(n1, n2); break;
-        case '/': res = quotient(n1, n2); break;
-        case '%': res = modulo(n1, n2); break;
-        case '&': res = et_bit(n1, n2); break;
-        case '|': res = ou_bit(n1, n2); break;
-        default:
-            printf("Erreur : Operateur '%c' inconnu.\n", op);
-            return 1;
+    } else {
+        if (argc < 4) { printf("Erreur : Deuxieme nombre requis.\n"); return 1; }
+        int n2 = atoi(argv[3]);
+        switch (op) {
+            case '+': res = somme(n1, n2); break;
+            case '-': res = difference(n1, n2); break;
+            case '*': res = produit(n1, n2); break;
+            case '/': res = quotient(n1, n2); break;
+            case '%': res = modulo(n1, n2); break;
+            case '&': res = et_bit(n1, n2); break;
+            case '|': res = ou_bit(n1, n2); break;
+            default: printf("Operateur inconnu.\n"); return 1;
+        }
     }
 
     printf("Resultat : %d\n", res);
