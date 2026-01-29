9523bfe61747587394492c1ff8178d48542a191c
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 17:09:05 +0100

    Update operator.h

--- TP4/src/operator.h
+++ TP4/src/operator.h
@@ -1,13 +1,13 @@
 #ifndef OPERATOR_H
 #define OPERATOR_H
 
-int somme(int num1, int num2);
-int difference(int num1, int num2);
-int produit(int num1, int num2);
-int quotient(int num1, int num2);
-int modulo(int num1, int num2);
-int et_bit(int num1, int num2);
-int ou_bit(int num1, int num2);
-int negation_bit(int num1);
+int somme(int n1, int n2);
+int difference(int n1, int n2);
+int produit(int n1, int n2);
+int quotient(int n1, int n2);
+int modulo(int n1, int n2);
+int et_bit(int n1, int n2);
+int ou_bit(int n1, int n2);
+int negation_bit(int n1);
 
 #endif
