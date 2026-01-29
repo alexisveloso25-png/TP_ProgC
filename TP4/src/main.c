d12ec5970201e043ae5b74677329e607854375af
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 12:03:12 +0100

    Update main.c

--- TP4/src/main.c
+++ TP4/src/main.c
@@ -1,56 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "operator.h"
-#include "fichier.h" // Inclusion du nouvel en tête.
+#include "fichier.h"
+
+// Utilise bien des underscores (_) et pas d'espaces
+void exercice_4_1() {
+    int n1, n2, res;
+    char op;
+    printf("\n--- Exercice 4.1 : Calculatrice ---\n");
+    printf("Entrez num1 : "); fflush(stdout); scanf("%d", &n1);
+    printf("Entrez num2 : "); fflush(stdout); scanf("%d", &n2);
+    printf("Entrez l'operateur (+, -, *, /) : "); fflush(stdout); scanf(" %c", &op);
+
+    switch(op) {
+        case '+': res = somme(n1, n2); break;
+        case '-': res = difference(n1, n2); break;
+        // ... autres cas ...
+        default: printf("Erreur\n"); return;
+    }
+    printf("Resultat : %d\n", res);
+}
 
 void exercice_4_2() {
     int choix;
-    char nom_f[100];
-    char msg[256];
-
-    printf("\nQue souhaitez-vous faire ?\n");
-    printf("1. Lire un fichier\n");
-    printf("2. Ecrire dans un fichier\n");
-    printf("Votre choix : ");
-    fflush(stdout);
+    char nom[100];
+    printf("\n1. Lire\n2. Ecrire\nChoix : "); fflush(stdout);
     scanf("%d", &choix);
-    getchar(); // Consomme le retour à la ligne restant
-
-    if (choix == 1) {
-        printf("Entrez le nom du fichier a lire : ");
-        fflush(stdout);
-        scanf("%s", nom_f);
-        lire_fichier(nom_f); //
-    } 
-    else if (choix == 2) {
-        printf("Entrez le nom du fichier : ");
-        fflush(stdout);
-        scanf("%s", nom_f);
-        getchar(); // Nettoyage avant le message
-
-        printf("Entrez le message a ecrire : ");
-        fflush(stdout);
-        fgets(msg, sizeof(msg), stdin); // fgets permet de saisir des espaces
-        
-        ecrire_dans_fichier(nom_f, msg); //
-    } 
-    else {
-        printf("Choix invalide.\n");
+    if(choix == 1) {
+        printf("Nom du fichier : "); fflush(stdout);
+        scanf("%s", nom);
+        lire_fichier(nom);
     }
+    // ... reste du code ...
 }
 
 int main() {
     int choix;
-    printf("Menu de Travail :\n");
-    printf("1. Exercice 4.1 (Calculatrice)\n");
-    printf("2. Exercice 4.2 (Fichiers)\n");
-    printf("Votre choix : ");
-    fflush(stdout);
-    scanf("%d", &choix);
-
-    if (choix == 1) exercice_4_1();
-    else if (choix == 2) exercice_4_2(); // Appel de la nouvelle fonction
-    else printf("Choix non valide.\n");
-
+    printf("Quel exercice ? (1 ou 2) : ");
+    fflush(stdout); 
+    if (scanf("%d", &choix) == 1) {
+        if (choix == 1) exercice_4_1(); // Pas d'espace ici !
+        else if (choix == 2) exercice_4_2();
+    }
     return 0;
 }
