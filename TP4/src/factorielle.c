5e01ffa40e4caffe2b63873e7fc41f3c7ce91070
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 12:52:10 +0100

    Update factorielle.c

--- TP4/src/factorielle.c
+++ TP4/src/factorielle.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 
-// Définition de la fonction factorielle récursive
 int factorielle(int num) {
     if (num == 0) {
-        printf("fact(0): 1\n");
         return 1;
     } else {
-        int valeur = num * factorielle(num - 1);
-        printf("fact(%d): %d\n", num, valeur);
-        return (valeur);
+        return (num * factorielle(num - 1));
     }
 }
