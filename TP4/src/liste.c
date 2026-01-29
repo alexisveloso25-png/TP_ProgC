ffdd72699502df8707df005ad0b99bdcd2f9fee7
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 17:10:27 +0100

    Update liste.c

--- TP4/src/liste.c
+++ TP4/src/liste.c
@@ -3,28 +3,16 @@
 #include "liste.h"
 
 void init_liste(struct liste_couleurs *l) { l->premier = NULL; }
-
 void insertion(struct couleur *c, struct liste_couleurs *l) {
     struct noeud *n = malloc(sizeof(struct noeud));
-    n->data = *c;
-    n->suivant = l->premier;
-    l->premier = n;
+    n->data = *c; n->suivant = l->premier; l->premier = n;
 }
-
 void parcours(struct liste_couleurs *l) {
-    struct noeud *a = l->premier;
-    while (a) {
-        printf("Couleur RGB: (%d, %d, %d)\n", a->data.r, a->data.g, a->data.b);
-        a = a->suivant;
+    struct noeud *act = l->premier;
+    int i = 1;
+    while (act) {
+        printf(" \x1B[36m●\x1B[0m Couleur %02d | \x1B[31mR:%3d\x1B[0m \x1B[32mG:%3d\x1B[0m \x1B[34mB:%3d\x1B[0m\n", 
+                i++, act->data.r, act->data.g, act->data.b);
+        act = act->suivant;
     }
-}
-
-void exercice_4_7() {
-    struct liste_couleurs ma_liste;
-    init_liste(&ma_liste);
-    for(int i=0; i<10; i++) {
-        struct couleur c = {(unsigned char)(i*20), 0, (unsigned char)(255-i*20), 255};
-        insertion(&c, &ma_liste);
-    }
-    parcours(&ma_liste);
 }
