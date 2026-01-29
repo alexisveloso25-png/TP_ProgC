c140d2caac60d2dadb1ae6d12bc5ad01beb65598
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 11:50:34 +0100

    Update main.c

--- TP4/src/main.c
+++ TP4/src/main.c
@@ -2,17 +2,24 @@
 #include <stdlib.h>
 #include "operator.h"
 
-// --- Entrée pour l'exercice 4.1 ---
 void exercice_4_1() {
     int n1, n2, res;
     char op;
-    printf("\n--- Exercice 4.1 : Calcul avec opérateurs ---\n");
+    
+    printf("\n--- Exercice 4.1 : Calcul avec operateurs ---\n");
+    
+    // Affichage AVANT la saisie pour guider Alexis, Salmane, Kais et Mohamed
     printf("Entrez num1 : ");
+    fflush(stdout); 
     scanf("%d", &n1);
+    
     printf("Entrez num2 : ");
+    fflush(stdout);
     scanf("%d", &n2);
+    
     printf("Entrez l'operateur (+, -, *, /, %%, &, |, ~) : ");
-    scanf(" %c", &op);
+    fflush(stdout);
+    scanf(" %c", &op); // L'espace avant %c est vital pour eviter de sauter l'entree
 
     switch (op) {
         case '+': res = somme(n1, n2); break;
@@ -23,30 +30,21 @@ void exercice_4_1() {
         case '&': res = et_bit(n1, n2); break;
         case '|': res = ou_bit(n1, n2); break;
         case '~': res = negation_bit(n1); break;
-        default: printf("Operateur invalide.\n"); return;
+        default: printf("Erreur : Operateur non reconnu.\n"); return;
     }
+
     printf("Resultat : %d\n", res);
 }
 
-// --- Placeholders pour les exercices futurs ---
-void exercice_4_2() { printf("\nExercice 4.2 non encore implemente.\n"); }
-void exercice_4_7() { printf("\nExercice 4.7 non encore implemente.\n"); }
-
 int main() {
     int choix;
-
-    printf("Quel exercice souhaitez-vous resoudre ?\n");
-    printf("1. Exercice 4.1 (Calculatrice)\n");
-    printf("2. Exercice 4.2\n");
-    printf("3. Exercice 4.7\n");
-    printf("Votre choix : ");
+    printf("Choisissez l'exercice (1 pour la calculatrice) : ");
     scanf("%d", &choix);
 
-    switch (choix) {
-        case 1: exercice_4_1(); break;
-        case 2: exercice_4_2(); break;
-        case 3: exercice_4_7(); break;
-        default: printf("Choix indisponible.\n"); break;
+    if (choix == 1) {
+        exercice_4_1();
+    } else {
+        printf("Exercice non implemente.\n");
     }
 
     return 0;
