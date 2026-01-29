c1ee2885712a7a2c792e2c1db5fd7900a1a18539
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 17:09:45 +0100

    Update operator.c

--- TP4/src/operator.c
+++ TP4/src/operator.c
@@ -1,10 +1,9 @@
 #include "operator.h"
-
-int somme(int num1, int num2) { return num1 + num2; }
-int difference(int num1, int num2) { return num1 - num2; }
-int produit(int num1, int num2) { return num1 * num2; }
-int quotient(int num1, int num2) { return (num2 != 0) ? num1 / num2 : 0; }
-int modulo(int num1, int num2) { return (num2 != 0) ? num1 % num2 : 0; }
-int et_bit(int num1, int num2) { return num1 & num2; }
-int ou_bit(int num1, int num2) { return num1 | num2; }
-int negation_bit(int num1) { return ~num1; }
+int somme(int n1, int n2) { return n1 + n2; }
+int difference(int n1, int n2) { return n1 - n2; }
+int produit(int n1, int n2) { return n1 * n2; }
+int quotient(int n1, int n2) { return (n2 != 0) ? n1 / n2 : 0; }
+int modulo(int n1, int n2) { return (n2 != 0) ? n1 % n2 : 0; }
+int et_bit(int n1, int n2) { return n1 & n2; }
+int ou_bit(int n1, int n2) { return n1 | n2; }
+int negation_bit(int n1) { return ~n1; }
