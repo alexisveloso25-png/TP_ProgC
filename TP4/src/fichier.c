d610b1c38b2b00048f8a2d9a3803af2cae2df12a
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 12:05:49 +0100

    Update fichier.c

--- TP4/src/fichier.c
+++ TP4/src/fichier.c
@@ -1,34 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "fichier.h"
 
-// Affiche le contenu d'un fichier à l'écran.
 void lire_fichier(char *nom_de_fichier) {
     FILE *f = fopen(nom_de_fichier, "r");
     if (f == NULL) {
-        printf("Erreur : Impossible d'ouvrir le fichier %s.\n", nom_de_fichier);
+        printf("Erreur : Impossible d'ouvrir %s\n", nom_de_fichier);
         return;
     }
-
-    printf("Contenu du fichier %s :\n", nom_de_fichier);
     char c;
-    while ((c = fgetc(f)) != EOF) {
-        putchar(c);
-    }
+    printf("Contenu de %s :\n", nom_de_fichier);
+    while ((c = fgetc(f)) != EOF) putchar(c);
     printf("\n");
     fclose(f);
 }
 
-// Enregistre un message dans un fichier
 void ecrire_dans_fichier(char *nom_de_fichier, char *message) {
-    FILE *f = fopen(nom_de_fichier, "w"); // "w" pour écraser ou créer
-    if (f == NULL) {
-        printf("Erreur : Impossible de creer le fichier %s.\n", nom_de_fichier);
-        return;
+    FILE *f = fopen(nom_de_fichier, "w");
+    if (f != NULL) {
+        fprintf(f, "%s", message);
+        fclose(f);
+        printf("Message enregistre dans %s\n", nom_de_fichier);
     }
-
-    fprintf(f, "%s", message);
-    fclose(f);
-    printf("Le message a ete ecrit dans le fichier %s.\n", nom_de_fichier);
 }
-
