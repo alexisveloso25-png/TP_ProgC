114b6b0a56d09269864a4ed39cd3fd40e11f82f2
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 10:22:46 +0100

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,27 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    int compteur = 5; // Modifiez cette valeur (doit être < 10)
+    int compteur = 5;
+    int i = 1;
 
-    if (compteur >= 10) {
-        printf("Erreur : le compteur doit être inférieur à 10.\n");
-        return 1;
-    }
-
-    printf("--- Triangle avec boucle FOR ---\n");
-    for (int i = 1; i <= compteur; i++) {
-        for (int j = 1; j <= i; j++) {
-            // Conditions pour afficher une étoile :
-            // 1. Première colonne (j == 1)
-            // 2. Dernière colonne de la ligne (j == i)
-            // 3. Dernière ligne du triangle (i == compteur)
+    printf("\n--- Triangle avec boucle WHILE ---\n");
+    while (i <= compteur) {
+        int j = 1;
+        while (j <= i) {
             if (j == 1 || j == i || i == compteur) {
                 printf("* ");
             } else {
                 printf("# ");
             }
+            j++;
         }
         printf("\n");
+        i++;
     }
 
     return 0;
