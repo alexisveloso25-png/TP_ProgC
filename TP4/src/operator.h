b2d4f279b7c9e92d5315ebef6d1d997fdbbd3b72
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 12:05:08 +0100

    Update operator.h

--- TP4/src/operator.h
+++ TP4/src/operator.h
@@ -1,6 +1,6 @@
 #ifndef OPERATOR_H
 #define OPERATOR_H
-//
+
 int somme(int num1, int num2);
 int difference(int num1, int num2);
 int produit(int num1, int num2);
@@ -11,4 +11,3 @@ int ou_bit(int num1, int num2);
 int negation_bit(int num1);
 
 #endif
-
