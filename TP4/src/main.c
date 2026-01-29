14a646bd97dee55ea375f8897ccf3a225a4b2d45
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 12:56:46 +0100

    Update main.c

--- TP4/src/main.c
+++ TP4/src/main.c
@@ -4,12 +4,10 @@
 #include "fichier.h"
 #include "liste.h"
 
-// Prototypes des fonctions définies dans d'autres fichiers .c
-void exercice_4_1(); 
-void exercice_4_2();
-void exercice_4_3(); 
+// Prototypes des fonctions definies dans les autres fichiers .c
+void exercice_4_3();
 int factorielle(int n);
-void exercice_4_6(char *nom);
+void exercice_4_6(char *f);
 void exercice_4_7();
 
 int main(int argc, char *argv[]) {
@@ -20,39 +18,33 @@ int main(int argc, char *argv[]) {
 
     int choix;
     while (1) {
-        printf("\n\x1B[36m=== MENU TP4 - GROUPE MASTER ===\x1B[0m\n");
-        printf("1. Calculatrice | 2. Fichiers | 3. Etudiants\n");
-        printf("5. Factorielle  | 6. Recherche | 7. Liste Couleurs | 0. Quitter\n");
-        printf("Choix : "); fflush(stdout);
-
+        printf("\n=== MENU TP4 ===\n1. Calculatrice\n2. Fichiers\n3. Etudiants\n5. Factorielle\n7. Liste Couleurs\n0. Quitter\nChoix : ");
         if (scanf("%d", &choix) != 1 || choix == 0) break;
 
         switch (choix) {
-            case 1: exercice_4_1(); break;
-            case 2: exercice_4_2(); break;
-            case 3: exercice_4_3(); break;
-            case 5: {
-                int n; printf("Nombre : "); scanf("%d", &n);
-                if(n >= 0) printf("Resultat : %d\n", factorielle(n));
+            case 1: {
+                int a, b; char op;
+                printf("Entrez num1 op num2 (ex: 10 + 5) : ");
+                scanf("%d %c %d", &a, &op, &b);
+                if (op == '+') printf("Resultat : %d\n", somme(a, b));
+                else if (op == '*') printf("Resultat : %d\n", produit(a, b));
                 break;
             }
-            case 6: {
-                char f[50]; printf("Fichier : "); scanf("%s", f);
-                exercice_4_6(f); break;
+            case 2:
+                lire_fichier("test.txt");
+                break;
+            case 3:
+                exercice_4_3();
+                break;
+            case 5: {
+                int n; printf("Nombre : "); scanf("%d", &n);
+                printf("Fact(%d) = %d\n", n, factorielle(n));
+                break;
             }
-            case 7: exercice_4_7(); break;
+            case 7:
+                exercice_4_7();
+                break;
         }
     }
     return 0;
-}
-
-// Implémentations locales simples
-void exercice_4_1() {
-    int a, b; printf("Somme de deux nombres : "); scanf("%d %d", &a, &b);
-    printf("Resultat : %d\n", somme(a, b));
-}
-
-void exercice_4_2() {
-    char nom[50]; printf("Lire quel fichier ? "); scanf("%s", nom);
-    lire_fichier(nom);
 }
