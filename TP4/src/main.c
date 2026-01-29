9da53840d516963001fcbee0b6f0307d4a580c59
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-29 12:46:19 +0100

    Update main.c

--- TP4/src/main.c
+++ TP4/src/main.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "operator.h"
 #include "fichier.h"
 #include "liste.h"
 
-// --- PROTOTYPES ---
-// On déclare les fonctions ici pour que le main puisse les appeler
-void exercice_4_1();           // Dans operator.c (ou fichier dédié)
-void exercice_4_2();           // Dans fichier.c
-void exercice_4_3();           // Dans etudiant_bd.c
-int factorielle(int n);        // Dans factorielle.c
-void exercice_4_6(char *nom);  // Dans chercherfichier.c
-void exercice_4_7();           // Dans liste.c
+// Prototypes des fonctions définies dans d'autres fichiers .c
+void exercice_4_1(); 
+void exercice_4_2();
+void exercice_4_3(); 
+int factorielle(int n);
+void exercice_4_6(char *nom);
+void exercice_4_7();
 
 int main(int argc, char *argv[]) {
     if (argc == 2) {
@@ -22,17 +20,10 @@ int main(int argc, char *argv[]) {
 
     int choix;
     while (1) {
-        printf("\n\x1B[36m==============================================\n");
-        printf("   MENU TP4 - GROUPE MASTER (Visuel Corrigé)\n");
-        printf("==============================================\x1B[0m\n");
-        printf("1. Calculatrice Interactive\n");
-        printf("2. Gestion Fichiers Simple\n");
-        printf("3. Base Donnees Etudiants\n");
-        printf("5. Factorielle Recursive\n");
-        printf("6. Recherche dans un Fichier\n");
-        printf("7. Liste Couleurs RGB\n");
-        printf("0. Quitter\n");
-        printf("\n\x1B[33m> Choix : \x1B[0m"); fflush(stdout);
+        printf("\n\x1B[36m=== MENU TP4 - GROUPE MASTER ===\x1B[0m\n");
+        printf("1. Calculatrice | 2. Fichiers | 3. Etudiants\n");
+        printf("5. Factorielle  | 6. Recherche | 7. Liste Couleurs | 0. Quitter\n");
+        printf("Choix : "); fflush(stdout);
 
         if (scanf("%d", &choix) != 1 || choix == 0) break;
 
@@ -50,10 +41,18 @@ int main(int argc, char *argv[]) {
                 exercice_4_6(f); break;
             }
             case 7: exercice_4_7(); break;
-            default: printf("Choix invalide.\n");
         }
-        printf("\nAppuyez sur Entree pour continuer...");
-        getchar(); getchar(); 
     }
     return 0;
+}
+
+// Implémentations locales simples
+void exercice_4_1() {
+    int a, b; printf("Somme de deux nombres : "); scanf("%d %d", &a, &b);
+    printf("Resultat : %d\n", somme(a, b));
+}
+
+void exercice_4_2() {
+    char nom[50]; printf("Lire quel fichier ? "); scanf("%s", nom);
+    lire_fichier(nom);
 }
