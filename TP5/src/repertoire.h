ef52a6f663d13d94559e36e5fb8ff1304679b781
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-30 10:25:46 +0100

    Update repertoire.h

--- TP5/src/repertoire.h
+++ TP5/src/repertoire.h
@@ -7,4 +7,10 @@ void lire_dossier(char *nom_repertoire);
 // Exercice 5.2 : Liste récursive
 void lire_dossier_recursif(char *nom_repertoire);
 
+/**
+ * Exercice 5.3 : Liste itérative complète
+ * Utilise une pile pour parcourir l'arborescence sans récursion.
+ */
+void lire_dossier_iteratif(char *nom_repertoire);
+
 #endif
