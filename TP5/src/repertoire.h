b27e580cf7afefcf11909e0947ca9bee3b35381c
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-30 10:13:22 +0100

    Update repertoire.h

---
+++ TP5/src/repertoire.h
@@ -0,0 +1,16 @@
+#ifndef REPERTOIRE_H
+#define REPERTOIRE_H
+
+/**
+ * Exercice 5.1 : Liste simple des fichiers et répertoires.
+ *
+ */
+void lire_dossier(char *nom_repertoire);
+
+/**
+ * Exercice 5.2 : Parcours récursif de toute l'arborescence.
+ *
+ */
+void lire_dossier_recursif(char *nom_repertoire);
+
+#endif
