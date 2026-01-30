f0a8d2ac7ffe56880d3f622c9115ce9a59f0edf5
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-30 10:22:27 +0100

    Update repertoire.h

--- TP5/src/repertoire.h
+++ TP5/src/repertoire.h
@@ -1,16 +1,10 @@
 #ifndef REPERTOIRE_H
 #define REPERTOIRE_H
 
-/**
- * Exercice 5.1 : Liste simple
- * Ouvre un répertoire et affiche son contenu immédiat (fichiers et dossiers).
- */
+// Exercice 5.1 : Liste simple
 void lire_dossier(char *nom_repertoire);
 
-/**
- * Exercice 5.2 : Liste récursive
- * Parcourt toute l'arborescence à partir d'un point donné, incluant les sous-dossiers.
- */
+// Exercice 5.2 : Liste récursive
 void lire_dossier_recursif(char *nom_repertoire);
 
 #endif
