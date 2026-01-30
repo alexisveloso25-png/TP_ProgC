07d1fda721b72056bb07f56504882fdbac614429
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-30 10:26:15 +0100

    Update tester_tp5.c

--- TP5/src/tester_tp5.c
+++ TP5/src/tester_tp5.c
@@ -2,20 +2,15 @@
 #include "repertoire.h"
 
 int main(int argc, char *argv[]) {
-    // Vérification que l'utilisateur a donné un dossier en argument
-    if (argc < 2) {
-        printf("\x1B[31mUsage: %s <nom_du_repertoire>\x1B[0m\n", argv[0]);
-        printf("Exemple: %s .\n", argv[0]);
+    if (argc != 2) {
+        printf("Utilisation : %s <nom_du_repertoire>\n", argv[0]);
         return 1;
     }
 
-    char *cible = argv[1];
+    char *nom_repertoire = argv[1];
 
-    printf("\n\x1B[36m=== TEST EXERCICE 5.1 (LISTE SIMPLE) ===\x1B[0m\n");
-    lire_dossier(cible);
-
-    printf("\n\x1B[36m=== TEST EXERCICE 5.2 (LISTE RECURSIVE) ===\x1B[0m\n");
-    lire_dossier_recursif(cible);
+    printf("\n>>> Lancement de l'exercice 5.3 (Iteratif) <<<\n");
+    lire_dossier_iteratif(nom_repertoire);
 
     return 0;
 }
